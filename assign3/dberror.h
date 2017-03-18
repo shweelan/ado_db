ee3d5fda81e9200d93808cfddbf33f6f0bc984e1
Shweelan Samson <shweelan@genieacs.com>
2017-03-18 18:07:12 -0500

    added const file

--- assign3/dberror.h
+++ assign3/dberror.h
@@ -2,36 +2,8 @@
 #define DBERROR_H
 
 #include "stdio.h"
+#include "const.h"
 #include <errno.h>
-
-/*
-  choosing 8192 as block size, because most of the operating systems are implementing blocks of size of power of 2, mostly 4096 or 8192
-  reading smaller or larger that are not power of 2 blocks will waste bytes already feched by the file system
-*/
-/* module wide constants */
-#define PAGE_SIZE 8192
-
-/* Schema Stringify delimiter */
-#define DELIMITER ((char *) ",")
-
-/* Per table buffer size */
-#define PER_TBL_BUF_SIZE 10
-
-/* Page header length */
-#define PAGE_HEADER_LEN 11
-
-/* bytes to represent number of slots in page */
-#define BYTES_SLOTS_COUNT 2
-
-/* Table header length */
-#define TABLE_HEADER_PAGES_LEN 2
-
-/* Number of bits in byte */
-#define NUM_BITS 8
-
-/* DB path configuration */
-#define PATH_DIR "/tmp/database_ado/"
-#define DEFAULT_MODE 0777
 
 /* return code definitions */
 typedef int RC;
