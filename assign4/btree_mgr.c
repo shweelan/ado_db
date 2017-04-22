7fad56bbbd8fe8b356d9c21ad0f646a6f209f6ab
Shweelan Samson <shweelan@genieacs.com>
2017-04-21 19:37:28 -0500

    Add loadtree from file

--- assign4/btree_mgr.c
+++ assign4/btree_mgr.c
@@ -4,6 +4,7 @@
 #include "storage_mgr.h"
 #include "buffer_mgr.h"
 #include <stdarg.h>
+#include <unistd.h>
 
 // helpers
 
@@ -43,6 +44,55 @@ void destroyBTNode(BT_Node *node) {
     saDestroy(node->childrenPages);
   }
   free(node);
+}
+
+
+RC deleteBtree (char *idxId) {
+  if(access(idxId, F_OK) == -1) {
+    return RC_FILE_NOT_FOUND;
+  }
+  int ret = unlink(idxId);
+  if (ret == -1) {
+    return RC_FS_ERROR;
+  }
+  return RC_OK;
+}
+
+
+void printKeyType(DataType dataType){
+  switch(dataType)
+  {
+    case DT_INT:
+      printf("DT_INT\n");
+      break;
+    case DT_FLOAT:
+      printf("DT_FLOAT\n");
+      break;
+    case DT_STRING:
+      printf("DT_STRING\n");
+      break;
+    case DT_BOOL:
+      printf("DT_BOOL\n");
+      break;
+  }
+}
+
+
+RC getNumNodes (BTreeHandle *tree, int *result) {
+  *result = tree->numNodes;
+  return RC_OK;
+}
+
+
+RC getNumEntries (BTreeHandle *tree, int *result) {
+  *result = tree->numEntries;
+  return RC_OK;
+}
+
+
+RC getKeyType (BTreeHandle *tree, DataType *result) {
+  *result = tree->keyType;
+  return RC_OK;
 }
 
 
@@ -140,27 +190,31 @@ RC writeNode(BT_Node *node, BTreeHandle *tree, int pageNum) {
 }
 
 
-void printNode(BT_Node *nodeNew) {
+void printNode(BT_Node *node) {
+  if (node == NULL) {
+    printf("NULL Node !!\n");
+    return;
+  }
   printf("\nNode details==>\n");
-  printf("Is Leaf : %d\t Size: %d\t Filled: %d \nNodeData:\t [ ", nodeNew->isLeaf, nodeNew->size, nodeNew->vals->fill);
+  printf("Is Leaf : %d\t Size: %d\t Filled: %d \nNodeData:\t [ ", node->isLeaf, node->size, node->vals->fill);
   int i;
-  if(nodeNew->isLeaf){
-    for (i = 0; i < nodeNew->vals->fill; i++) {
-      printf("%d", nodeNew->leafRIDPages->elems[i]);
-      printf(".%d , ", nodeNew->leafRIDSlots->elems[i]);
-      printf("<%d> ", nodeNew->vals->elems[i]);
-      if(i != nodeNew->vals->fill-1){
+  if(node->isLeaf){
+    for (i = 0; i < node->vals->fill; i++) {
+      printf("%d", node->leafRIDPages->elems[i]);
+      printf(".%d , ", node->leafRIDSlots->elems[i]);
+      printf("<%d> ", node->vals->elems[i]);
+      if(i != node->vals->fill-1){
         printf(" ,");
       }
     }
     printf("]");
 
   } else {
-    for (i = 0; i < nodeNew->vals->fill; i++) {
-      printf("%d , ", nodeNew->childrenPages->elems[i]);
-      printf("<%d> , ", nodeNew->vals->elems[i]);
+    for (i = 0; i < node->vals->fill; i++) {
+      printf("%d , ", node->childrenPages->elems[i]);
+      printf("<%d> , ", node->vals->elems[i]);
     }
-    printf("%d ]", nodeNew->childrenPages->elems[i]);
+    printf("%d ]", node->childrenPages->elems[i]);
   }
   printf("\n------------\n");
 }
@@ -197,20 +251,21 @@ RC loadBtreeNodes(BTreeHandle *tree, BT_Node *root, BT_Node **leftOnLevel, int l
   BT_Node *child;
   BT_Node *left = leftOnLevel[level];
   RC err;
+//  printf("######################### LEVEL = %d\n", level);
+//  printNode(root);
   if(!root->isLeaf) {
     for (int i = 0; i < root->childrenPages->fill; i++) {
-      child = root->children[i];
-      if ((err = readNode(&child, tree, root->childrenPages->elems[i]))) {
+      if ((err = readNode(&root->children[i], tree, root->childrenPages->elems[i]))) {
         return err;
       }
       if (left != NULL) {
-        left->right = child;
+        left->right = root->children[i];
       }
-      child->left = left;
-      left = child;
-      child->parent = root;
+      root->children[i]->left = left;
+      left = root->children[i];
+      root->children[i]->parent = root;
       leftOnLevel[level] = left;
-      if ((err = loadBtreeNodes(tree, child, leftOnLevel, i + 1))) {
+      if ((err = loadBtreeNodes(tree, root->children[i], leftOnLevel, level + 1))) {
         return err;
       }
     }
@@ -218,6 +273,53 @@ RC loadBtreeNodes(BTreeHandle *tree, BT_Node *root, BT_Node **leftOnLevel, int l
   return RC_OK;
 }
 
+
+RC loadBtree(BTreeHandle *tree) {
+  RC err;
+  tree->root = NULL;
+  if (tree->depth) {
+    if ((err = readNode(&tree->root, tree, tree->whereIsRoot))) {
+      return err;
+    }
+    BT_Node **leftOnLevel = newArray(BT_Node *, tree->depth);
+    for (int i = 0; i < tree->depth; i++) {
+      leftOnLevel[i] = NULL;
+    }
+    if ((err = loadBtreeNodes(tree, tree->root, leftOnLevel, 0))) {
+      return err;
+    }
+  }
+  printf("depth = %d\n", tree->depth);
+  return RC_OK;
+}
+
+
+RC writeBtreeHeader(BTreeHandle *tree) {
+  RC err;
+  BM_BufferPool *bm = tree->mgmtData;
+  BM_PageHandle *page = new(BM_PageHandle); // TODO free it
+  if (RC_OK != (err = pinPage(bm, page, 0))) {
+    freeit(1, page);
+    return err;
+  }
+  char *ptr = page->data;
+  memcpy(ptr, &tree->size, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(ptr, &tree->keyType, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(ptr, &tree->whereIsRoot, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(ptr, &tree->numNodes, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(ptr, &tree->numEntries, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(ptr, &tree->depth, SIZE_INT);
+  err = markDirty(bm, page);
+  err = unpinPage(bm, page);
+  forceFlushPool(bm);
+  freeit(1, page);
+  return err;
+}
 
 //functionality
 
@@ -273,6 +375,7 @@ RC createBtree (char *idxId, DataType keyType, int n)
   return rc;
 }
 
+
 RC openBtree (BTreeHandle **tree, char *idxId){
   BTreeHandle *_tree = new(BTreeHandle);
   RC err;
@@ -281,7 +384,6 @@ RC openBtree (BTreeHandle **tree, char *idxId){
     freeit(2, bm, _tree);
     return err;
   }
-
   BM_PageHandle *pageHandle = new(BM_PageHandle); //TODO: free it, Done below
   if (RC_OK != (err = pinPage(bm, pageHandle, 0))) {
     freeit(3, bm, pageHandle, _tree);
@@ -309,21 +411,12 @@ RC openBtree (BTreeHandle **tree, char *idxId){
     return err;
   }
   freeit(1, pageHandle);
-
-  if (_tree->depth) {
-    if ((err = readNode(&_tree->root, _tree, _tree->whereIsRoot))) {
-      closeBtree(_tree);
-      return err;
-    }
-    BT_Node **leftOnLevel = newArray(BT_Node *, _tree->depth);
-    for (int i = 0; i < _tree->depth; i++) {
-      leftOnLevel[i] = NULL;
-    }
-    if ((err = loadBtreeNodes(_tree, _tree->root, leftOnLevel, 0))) {
-      closeBtree(_tree);
-      return err;
-    }
+  ///*  Coment me
+  if ((err = loadBtree(_tree))) {
+    closeBtree(_tree);
+    return err;
   }
+  //*/
   *tree = _tree;
   return RC_OK;
 }
@@ -336,9 +429,204 @@ RC closeBtree (BTreeHandle *tree){
   return RC_OK;
 }
 
+void saveTestLeaf2(BTreeHandle *tree,int pn, int val, int i, int fill){
+  int dummy = 2912;
+  int lvals[2] = {val, dummy};
+  int ridP[2] = {i, dummy};
+  int ridS[2] = {i, dummy};
+  gwl(tree, pn, 2, fill, &lvals[0], &ridP[0], &ridS[0]);
+  i++;
+}
+
+void saveTestNode2(BTreeHandle *tree,int pn, int val, int i, int j, int fill){
+  int dummy = 2912;
+  int lvals[2] = {val,dummy};
+  int ptr[3] = {i, j, dummy};
+  gwn(tree, pn, 2, fill, &lvals[0], &ptr[0]);
+  i++;
+}
+
+RC createTestBTree2(BTreeHandle *tree){
+  int fill = 2;
+  int pn = 1;
+  int lvals1[2] = {1, 20};
+  int ridP1[2] = {0,1};
+  int ridS1[2] = {0,1};
+  gwl(tree, pn, 2, fill, &lvals1[0], &ridP1[0], &ridS1[0]);
+
+  int i=2;
+  fill = 1;
+  pn = 27;
+  int val = 25;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 26;
+  val = 28;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 24;
+  val = 29;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 23;
+  val = 30;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 20;
+  val = 33;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 19;
+  val = 35;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 17;
+  val = 39;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 16;
+  val = 40;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 12;
+  val = 43;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 11;
+  val = 45;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 9;
+  val = 49;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 8;
+  val = 50;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 5;
+  val = 52;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 4;
+  val = 60;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  pn = 2;
+  val = 70;
+  saveTestLeaf2(tree, pn,val, i, fill);
+  i++;
+
+  //-----------------------------------------------
+  pn = 3;
+  val = 25;
+  int prevPage=1;
+  int nextPage=27;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 28;
+  val = 29;
+  prevPage=26;
+  nextPage=24;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 25;
+  val = 33;
+  prevPage=23;
+  nextPage=20;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 21;
+  val = 39;
+  prevPage=19;
+  nextPage=17;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 18;
+  val = 43;
+  prevPage=16;
+  nextPage=12;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 13;
+  val = 49;
+  prevPage=11;
+  nextPage=9;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 10;
+  val = 52;
+  prevPage=8;
+  nextPage=5;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 6;
+  val = 70;
+  prevPage=4;
+  nextPage=2;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 7;
+  val = 28;
+  prevPage=3;
+  nextPage=28;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 29;
+  val = 35;
+  prevPage=25;
+  nextPage=21;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 22;
+  val = 45;
+  prevPage=18;
+  nextPage=13;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 14;
+  val = 60;
+  prevPage=10;
+  nextPage=6;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 15;
+  val = 30;
+  prevPage=7;
+  nextPage=29;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 30;
+  val = 50;
+  prevPage=22;
+  nextPage=14;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  pn = 31;
+  val = 40;
+  prevPage=15;
+  nextPage=30;
+  saveTestNode2(tree, pn, val, prevPage, nextPage, fill);
+
+  return RC_OK;
+}
 
 int main () {
-  ///*
+  /*
   smartArray *arr = saInit(20);
   saInsert(arr, 17);
   saDeleteOne(arr, 17);
@@ -376,15 +664,14 @@ int main () {
   }
   saDestroy(arr);
   //*/
-  int n = 3;
+  int n = 2;
   char *name = "myIndex";
   createBtree (name, DT_INT, n);
-  BTreeHandle *tree = new(BTreeHandle);
-  tree->size = n;
+  BTreeHandle *tree;
   openBtree(&tree, name);
 
   //----------------Non Leaf Node-------------------------------
-  ///*
+  /*
   BT_Node *node = createBTNode(4, 0);
   int index;
   index = saInsert(node->vals, 1214237423);
@@ -398,7 +685,7 @@ int main () {
   destroyBTNode(node);
   //*/
 
-  ///*
+  /*
   BT_Node *nodeNew = NULL;
   readNode(&nodeNew, tree, 1);
   printNode(nodeNew);
@@ -407,7 +694,7 @@ int main () {
   //-----------------------------------------------
 
   //--------------Leaf Node---------------------------------
-  ///*
+  /*
   node = createBTNode(4, 1);
   index = saInsert(node->vals, 1244324234);
   saInsertAt(node->leafRIDPages, 2, index);
@@ -422,35 +709,40 @@ int main () {
   destroyBTNode(node);
   //*/
 
-  ///*
+  /*
   readNode(&nodeNew, tree, 2);
   printNode(nodeNew);
   destroyBTNode(nodeNew);
   //*/
+
+
   //-----------------------------------------------
 
-  // example node
-  int dummy = 2912;
-  n = 5;
-  int pn = 1;
-  int fill = 4;
-  int vals[5] = {5, 7, 8, 10, dummy};
-  int ptrs[6] = {2, 6, 10, 4, 11, dummy};
+  createTestBTree2(tree);
+  tree->depth = 5;
+  tree->numNodes = 31;
+  tree->whereIsRoot = 31;
+  writeBtreeHeader(tree);
+  //-----------------------------------------------
 
-  gwn(tree, pn, n, fill, &vals[0], &ptrs[0]);
+  /*
+  int result;
+  getNumNodes(tree, &result);
+  printf("\nNum Nodes : %d",result);
 
-  // exmaple leaf
-  fill = 3;
-  pn = 2;
-  int lvals[5] = {1, 2, 3, dummy, dummy};
-  int ridP[5] = {5, 2, 6, dummy, dummy};
-  int ridS[5] = {5, 3, 5, dummy, dummy};
-  gwl(tree, pn, n, fill, &vals[0], &ridP[0], &ridS[0]);
+  getNumEntries(tree, &result);
+  printf("\nNum Entries : %d",result);
 
-  // TODO it is not tested
-  // TODO must write all nodes and leafes for the tree I mentioned on whats app, like the examples above, u will be cnahging the pn for every one
-  // TODO must know exactly(when u draw it on papers) where is the root and write its pageNum when u create the tree, change it manually for now
-
+  DataType dataType;
+  getKeyType(tree, &dataType);
+  printf("\nKey Type: ");
+  printKeyType(dataType);
+  //*/
+  //-----------------------------------------------
   closeBtree(tree);
+  BTreeHandle *fullTree;
+  openBtree(&fullTree, name);
+  printNode(fullTree->root->children[0]->children[1]->right);
+  //deleteBtree(name);
   return 0;
 }
