3d98902e02e0622d491a98a34c05a391e591629d
Shweelan Samson <shweelan@genieacs.com>
2017-04-23 13:33:37 -0500

    clean up

--- assign4/btree_mgr.c
+++ assign4/btree_mgr.c
@@ -36,7 +36,6 @@ BT_Node *createBTNode(int size, int isLeaf) {
   return node;
 }
 
-
 void destroyBTNode(BT_Node *node) {
   saDestroy(node->vals);
   if (node->isLeaf) {
@@ -48,56 +47,6 @@ void destroyBTNode(BT_Node *node) {
   }
   free(node);
 }
-
-
-RC deleteBtree (char *idxId) {
-  if(access(idxId, F_OK) == -1) {
-    return RC_FILE_NOT_FOUND;
-  }
-  int ret = unlink(idxId);
-  if (ret == -1) {
-    return RC_FS_ERROR;
-  }
-  return RC_OK;
-}
-
-
-void printKeyType(DataType dataType){
-  switch(dataType)
-  {
-    case DT_INT:
-      printf("DT_INT\n");
-      break;
-    case DT_FLOAT:
-      printf("DT_FLOAT\n");
-      break;
-    case DT_STRING:
-      printf("DT_STRING\n");
-      break;
-    case DT_BOOL:
-      printf("DT_BOOL\n");
-      break;
-  }
-}
-
-
-RC getNumNodes (BTreeHandle *tree, int *result) {
-  *result = tree->numNodes;
-  return RC_OK;
-}
-
-
-RC getNumEntries (BTreeHandle *tree, int *result) {
-  *result = tree->numEntries;
-  return RC_OK;
-}
-
-
-RC getKeyType (BTreeHandle *tree, DataType *result) {
-  *result = tree->keyType;
-  return RC_OK;
-}
-
 
 RC readNode(BT_Node **node, BTreeHandle *tree, int pageNum) {
   RC err;
@@ -191,7 +140,6 @@ RC writeNode(BT_Node *node, BTreeHandle *tree, int pageNum) {
   return err;
 }
 
-
 void printNode(BT_Node *node) {
   if (node == NULL) {
     printf("NULL Node !!\n");
@@ -234,7 +182,6 @@ RC gwn(BTreeHandle *tree, int pageNum, int size, int fill, int *vals, int *ptrs)
   return err;
 }
 
-
 RC gwl(BTreeHandle *tree, int pageNum, int size, int fill, int *vals, int *ridP, int *ridS) {
   BT_Node *node = createBTNode(size, 1);
   int index, i;
@@ -248,12 +195,9 @@ RC gwl(BTreeHandle *tree, int pageNum, int size, int fill, int *vals, int *ridP,
   return err;
 }
 
-
 RC loadBtreeNodes(BTreeHandle *tree, BT_Node *root, BT_Node **leftOnLevel, int level) {
   BT_Node *left = leftOnLevel[level];
   RC err;
-//  printf("######################### LEVEL = %d\n", level);
-//  printNode(root);
   if(!root->isLeaf) {
     for (int i = 0; i < root->childrenPages->fill; i++) {
       if ((err = readNode(&root->children[i], tree, root->childrenPages->elems[i]))) {
@@ -274,23 +218,17 @@ RC loadBtreeNodes(BTreeHandle *tree, BT_Node *root, BT_Node **leftOnLevel, int l
   return RC_OK;
 }
 
-RC findKey (BTreeHandle *tree, Value *key, RID *result) {
+BT_Node *findNodeByKey(BTreeHandle *tree, int key) {
   BT_Node *current = tree->root;
   int index, fitOn;
-  while(!current->isLeaf) {
-    index = saBinarySearch(current->vals, key->v.intV, &fitOn);
+  while(current != NULL && !current->isLeaf) {
+    index = saBinarySearch(current->vals, key, &fitOn);
     if (index >= 0) {
       fitOn += 1;
     }
     current = current->children[fitOn];
   }
-  index = saBinarySearch(current->vals, key->v.intV, &fitOn);
-  if (index < 0) {
-    return RC_IM_KEY_NOT_FOUND;
-  }
-  result->page = current->leafRIDPages->elems[index];
-  result->slot = current->leafRIDSlots->elems[index];
-  return RC_OK;
+  return current;
 }
 
 RC loadBtree(BTreeHandle *tree) {
@@ -311,7 +249,6 @@ RC loadBtree(BTreeHandle *tree) {
   printf("depth = %d\n", tree->depth);
   return RC_OK;
 }
-
 
 RC writeBtreeHeader(BTreeHandle *tree) {
   RC err;
@@ -338,114 +275,6 @@ RC writeBtreeHeader(BTreeHandle *tree) {
   forceFlushPool(bm);
   freeit(1, page);
   return err;
-}
-
-//functionality
-
-RC initIndexManager (void *mgmtData) {
-  return RC_OK;
-}
-
-
-RC shutdownIndexManager () {
-  return RC_OK;
-}
-
-
-RC createBtree (char *idxId, DataType keyType, int n)
-{
-  RC rc;
-  if(RC_OK!= (rc = createPageFile (idxId))){
-    return rc;
-  }
-
-  SM_FileHandle *fHandle = new(SM_FileHandle);// TODO free it, Done below
-  if (RC_OK != (rc = openPageFile(idxId, fHandle))) {
-    free(fHandle);
-    return rc;
-  }
-
-  char *header = newCleanArray(char, PAGE_SIZE);
-  char *ptr = header;
-  memcpy(ptr, &n, SIZE_INT);
-  ptr += SIZE_INT;
-  memcpy(ptr, &keyType, SIZE_INT);
-  ptr += SIZE_INT;
-  int whereIsRoot = 0;
-  memcpy(ptr, &whereIsRoot, SIZE_INT);
-  ptr += SIZE_INT;
-  int numNodes = 0;
-  memcpy(ptr, &numNodes, SIZE_INT);
-  ptr += SIZE_INT;
-  int numEntries = 0;
-  memcpy(ptr, &numEntries, SIZE_INT);
-  ptr += SIZE_INT;
-  int depth = 0;
-  memcpy(ptr, &depth, SIZE_INT);
-
-  if (RC_OK != (rc = writeBlock(0, fHandle, header))) {
-    free(fHandle);
-    free(header);
-    return rc;
-  }
-  free(header);
-  rc = closePageFile(fHandle);
-  free(fHandle);
-  return rc;
-}
-
-
-RC openBtree (BTreeHandle **tree, char *idxId){
-  BTreeHandle *_tree = new(BTreeHandle);
-  RC err;
-  BM_BufferPool *bm = new(BM_BufferPool); //TODO: free it in close
-  if ((err = initBufferPool(bm, idxId, PER_IDX_BUF_SIZE, RS_LRU, NULL))) {
-    freeit(2, bm, _tree);
-    return err;
-  }
-  BM_PageHandle *pageHandle = new(BM_PageHandle); //TODO: free it, Done below
-  if (RC_OK != (err = pinPage(bm, pageHandle, 0))) {
-    freeit(3, bm, pageHandle, _tree);
-    return err;
-  }
-  char *ptr = pageHandle->data;
-  _tree->idxId = idxId;
-  _tree->mgmtData = bm;
-
-  memcpy(&_tree->size, ptr, SIZE_INT);
-  ptr += SIZE_INT;
-  memcpy(&_tree->keyType, ptr, SIZE_INT);
-  ptr += SIZE_INT;
-  memcpy(&_tree->whereIsRoot, ptr, SIZE_INT);
-  ptr += SIZE_INT;
-  memcpy(&_tree->numNodes, ptr, SIZE_INT);
-  ptr += SIZE_INT;
-  memcpy(&_tree->numEntries, ptr, SIZE_INT);
-  ptr += SIZE_INT;
-  memcpy(&_tree->depth, ptr, SIZE_INT);
-
-  if ((err = unpinPage(bm, pageHandle)) != RC_OK) {
-    freeit(1, pageHandle);
-    closeBtree(_tree);
-    return err;
-  }
-  freeit(1, pageHandle);
-  ///*  Coment me
-  if ((err = loadBtree(_tree))) {
-    closeBtree(_tree);
-    return err;
-  }
-  //*/
-  *tree = _tree;
-  return RC_OK;
-}
-
-
-RC closeBtree (BTreeHandle *tree){
-  shutdownBufferPool(tree->mgmtData);
-  // TODO free all nodes
-  free(tree);
-  return RC_OK;
 }
 
 void saveTestLeaf2(BTreeHandle *tree,int pn, int val, int i, int fill){
@@ -644,6 +473,146 @@ RC createTestBTree2(BTreeHandle *tree){
   return RC_OK;
 }
 
+//functionality
+
+RC initIndexManager (void *mgmtData) {
+  return RC_OK;
+}
+
+RC shutdownIndexManager () {
+  return RC_OK;
+}
+
+RC createBtree (char *idxId, DataType keyType, int n)
+{
+  RC rc;
+  if(RC_OK!= (rc = createPageFile (idxId))){
+    return rc;
+  }
+
+  SM_FileHandle *fHandle = new(SM_FileHandle);// TODO free it, Done below
+  if (RC_OK != (rc = openPageFile(idxId, fHandle))) {
+    free(fHandle);
+    return rc;
+  }
+
+  char *header = newCleanArray(char, PAGE_SIZE);
+  char *ptr = header;
+  memcpy(ptr, &n, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(ptr, &keyType, SIZE_INT);
+  ptr += SIZE_INT;
+  int whereIsRoot = 0;
+  memcpy(ptr, &whereIsRoot, SIZE_INT);
+  ptr += SIZE_INT;
+  int numNodes = 0;
+  memcpy(ptr, &numNodes, SIZE_INT);
+  ptr += SIZE_INT;
+  int numEntries = 0;
+  memcpy(ptr, &numEntries, SIZE_INT);
+  ptr += SIZE_INT;
+  int depth = 0;
+  memcpy(ptr, &depth, SIZE_INT);
+
+  if (RC_OK != (rc = writeBlock(0, fHandle, header))) {
+    free(fHandle);
+    free(header);
+    return rc;
+  }
+  free(header);
+  rc = closePageFile(fHandle);
+  free(fHandle);
+  return rc;
+}
+
+RC openBtree (BTreeHandle **tree, char *idxId){
+  BTreeHandle *_tree = new(BTreeHandle);
+  RC err;
+  BM_BufferPool *bm = new(BM_BufferPool); //TODO: free it in close
+  if ((err = initBufferPool(bm, idxId, PER_IDX_BUF_SIZE, RS_LRU, NULL))) {
+    freeit(2, bm, _tree);
+    return err;
+  }
+  BM_PageHandle *pageHandle = new(BM_PageHandle); //TODO: free it, Done below
+  if (RC_OK != (err = pinPage(bm, pageHandle, 0))) {
+    freeit(3, bm, pageHandle, _tree);
+    return err;
+  }
+  char *ptr = pageHandle->data;
+  _tree->idxId = idxId;
+  _tree->mgmtData = bm;
+
+  memcpy(&_tree->size, ptr, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(&_tree->keyType, ptr, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(&_tree->whereIsRoot, ptr, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(&_tree->numNodes, ptr, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(&_tree->numEntries, ptr, SIZE_INT);
+  ptr += SIZE_INT;
+  memcpy(&_tree->depth, ptr, SIZE_INT);
+
+  if ((err = unpinPage(bm, pageHandle)) != RC_OK) {
+    freeit(1, pageHandle);
+    closeBtree(_tree);
+    return err;
+  }
+  freeit(1, pageHandle);
+  if ((err = loadBtree(_tree))) {
+    closeBtree(_tree);
+    return err;
+  }
+  *tree = _tree;
+  return RC_OK;
+}
+
+RC closeBtree (BTreeHandle *tree){
+  shutdownBufferPool(tree->mgmtData);
+  // TODO free all nodes
+  free(tree);
+  return RC_OK;
+}
+
+RC deleteBtree (char *idxId) {
+  if(access(idxId, F_OK) == -1) {
+    return RC_FILE_NOT_FOUND;
+  }
+  int ret = unlink(idxId);
+  if (ret == -1) {
+    return RC_FS_ERROR;
+  }
+  return RC_OK;
+}
+
+RC getNumNodes (BTreeHandle *tree, int *result) {
+  *result = tree->numNodes;
+  return RC_OK;
+}
+
+RC getNumEntries (BTreeHandle *tree, int *result) {
+  *result = tree->numEntries;
+  return RC_OK;
+}
+
+RC getKeyType (BTreeHandle *tree, DataType *result) {
+  *result = tree->keyType;
+  return RC_OK;
+}
+
+RC findKey (BTreeHandle *tree, Value *key, RID *result) {
+  int index, fitOn;
+  BT_Node *leaf = findNodeByKey(tree, key->v.intV);
+  index = saBinarySearch(leaf->vals, key->v.intV, &fitOn);
+  if (index < 0) {
+    return RC_IM_KEY_NOT_FOUND;
+  }
+  result->page = leaf->leafRIDPages->elems[index];
+  result->slot = leaf->leafRIDSlots->elems[index];
+  return RC_OK;
+}
+
 RC openTreeScan (BTreeHandle *tree, BT_ScanHandle **handle){
   BT_ScanHandle *_handle = new(BT_ScanHandle);
   ScanMgmtInfo *scanInfo = new(ScanMgmtInfo);
@@ -770,7 +739,6 @@ int main () {
   destroyBTNode(nodeNew);
   //*/
 
-
   //-----------------------------------------------
 
   createTestBTree2(tree);
@@ -790,8 +758,6 @@ int main () {
 
   DataType dataType;
   getKeyType(tree, &dataType);
-  printf("\nKey Type: ");
-  printKeyType(dataType);
   //*/
   //-----------------------------------------------
   closeBtree(tree);
